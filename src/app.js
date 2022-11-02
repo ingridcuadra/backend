eb498a336c2e9ee80e11db7aec1ff3e7337e0e28
Ingrid Cuadra <ingrid_cuadra@outlook.es>
2022-11-02 20:03:41 -0300

    Servidor con Express

--- src/app.js
+++ src/app.js
@@ -1,13 +1,43 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 
 const server = app.listen(8080, ()=>console.log("Escuchando Express :)"))
 
 app.get('/', (req, res)=>{
-    res.send('hola express')
+    res.send('Bienvenid@s')
 })
 
-app.get('/despedida', (req, res)=>{
-    res.send('chau')
+const returnProducts = (ruta) => {
+    if (fs.existsSync(ruta)) {
+        let data = fs.readFileSync(ruta, 'utf-8')
+        let products = JSON.parse(data)
+        return products;
+    } else {
+        return console.log("Error leyendo datos")
+    }
+}
+
+app.get('/productos', (req, res) => {
+    res.send(returnProducts('./productos.json'))
+})
+app.get('/productoRandom', (req, res) => {
+    let products = returnProducts('./productos.json')
+    let numRandom = parseInt(Math.random() * products.length)
+    if (!products.status) {
+        res.send(products[numRandom])
+    } else {
+        res.send(products)
+    }
+})
+app.get('/producto/:idProduct', (req, res) => {
+    const id = req.params.idProduct
+    let products = returnProducts('./productos.json')
+    let product = products.find((product) => product.id == id);
+    if (product) {
+        res.send(product)
+    } else {
+        res.send("Producto no encontrado")
+    }
 })
