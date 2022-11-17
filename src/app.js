fdb0ec53b479922ee8c381ab8eeac064ca42e832
Ingrid Cuadra <ingrid_cuadra@outlook.es>
2022-11-17 01:06:40 -0300

    API RESTful

--- src/app.js
+++ src/app.js
@@ -1,43 +1,12 @@
 import express from 'express';
-import fs from 'fs';
+import __dirname from './utils.js';
+import productRouter from './routes/productos.router.js';
 
 const app = express();
 
 const server = app.listen(8080, ()=>console.log("Escuchando Express :)"))
 
-app.get('/', (req, res)=>{
-    res.send('Bienvenid@s')
-})
+app.use(express.json());
+app.use(express.static(__dirname+'/public'))
 
-const returnProducts = (ruta) => {
-    if (fs.existsSync(ruta)) {
-        let data = fs.readFileSync(ruta, 'utf-8')
-        let products = JSON.parse(data)
-        return products;
-    } else {
-        return console.log("Error leyendo datos")
-    }
-}
-
-app.get('/productos', (req, res) => {
-    res.send(returnProducts('./productos.json'))
-})
-app.get('/productoRandom', (req, res) => {
-    let products = returnProducts('./productos.json')
-    let numRandom = parseInt(Math.random() * products.length)
-    if (!products.status) {
-        res.send(products[numRandom])
-    } else {
-        res.send(products)
-    }
-})
-app.get('/producto/:idProduct', (req, res) => {
-    const id = req.params.idProduct
-    let products = returnProducts('./productos.json')
-    let product = products.find((product) => product.id == id);
-    if (product) {
-        res.send(product)
-    } else {
-        res.send("Producto no encontrado")
-    }
-})
+app.use('/api/productos', productRouter);
---
+++ src/routes/productos.router.js
@@ -0,0 +1,75 @@
+import { Router } from "express";
+import fs from 'fs';
+
+const productRouter = Router();
+
+const returnProducts = (ruta) => {
+    if (fs.existsSync(ruta)) {
+        let data = fs.readFileSync(ruta, 'utf-8')
+        let products = JSON.parse(data)
+        return products;
+    } else {
+        return console.log("Error leyendo datos")
+    }
+}
+
+productRouter.get('/', (req, res) => {
+    res.send(returnProducts('./productos.json'))
+})
+
+productRouter.get('/:idProduct', (req, res) => {
+    const id = req.params.idProduct
+    let products = returnProducts('./productos.json')
+    let product = products.find((product) => product.id == id);
+    if (product) {
+        res.send(product)
+    } else {
+        res.send("Producto no encontrado")
+    }
+})
+
+productRouter.post('/', (req, res) => {
+    const productToInsert = req.body.producto;
+    let products = returnProducts('./productos.json')
+    products.push(productToInsert);
+    res.send({status:"Producto agregado", payload:productToInsert});
+})
+
+//Producto para usar con POST
+// {
+//     "producto":{
+//         "id": 4,
+// 		   "title": "Foquita",
+// 		   "price": 199.99,
+// 		   "thumbnail": "https://drive.google.com/uc?export=view&id=142BcrojPC52ZiCC76rjSJhFWM-ZOe101"
+//     }
+// }
+
+productRouter.put('/:idProduct', (req, res) => {
+    const id = req.params.idProduct
+    const productToUpdate = req.body.producto;
+    res.send({status:"Producto actualizado", payload:productToUpdate});
+})
+
+//Producto para usar con PUT
+// {
+//     "producto":{
+//         "id": 4,
+// 		   "title": "Foquita",
+// 		   "price": 200.99,
+// 		   "thumbnail": "https://drive.google.com/uc?export=view&id=142BcrojPC52ZiCC76rjSJhFWM-ZOe101"
+//     }
+// }
+
+productRouter.delete('/:idProduct', (req, res) => {
+    const id = req.params.idProduct
+    let products = returnProducts('./productos.json')
+    let product = products.filter((product) => product.id != id);
+    if (product) {
+        res.send(product)
+    } else {
+        res.send("Producto no existente")
+    }
+})
+
+export default productRouter;
