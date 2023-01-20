2b2e9b629f014b467080233fdbd87e7bfec73f82
Ingrid Cuadra <ingrid_cuadra@outlook.es>
2023-01-20 20:09:34 -0300

    pug

--- src/app.js
+++ src/app.js
@@ -1,24 +1,42 @@
 import express from 'express';
 import __dirname from './utils.js';
 import productRouter from './routes/productos.router.js';
-import handlerbars from 'express-handlebars';
-import viewRouter from './routes/views.router.js';
 
 const app = express();
-const server = app.listen(8080, ()=>console.log("Escuchando Express :)"));
+const server = app.listen(8080,()=>console.log("Escuchando Express :)"));
+
+app.set('views',__dirname+'/views');
+app.set('view engine','pug');
+
+app.get('/',(req,res)=>{
+    res.render('home.pug',{
+        message:"Bienvenid@s a esta página principal con pug"
+    })
+})
+
+app.use('/productos', productRouter);
+
+// import express from 'express';
+// import __dirname from './utils.js';
+// import productRouter from './routes/productos.router.js';
+// import handlebars from 'express-handlebars';
+// import viewRouter from './routes/views.router.js';
+
+// const app = express();
+// const server = app.listen(8080, ()=>console.log("Escuchando Express :)"));
 
 // app.use((req,res,next)=>{
 //     console.log("Petición recibida");
 //     next();
 // })
 
-app.engine('handlerbars', handlerbars.engine());
-app.set('views', __dirname+'/views');
-app.set('view engine', 'handlerbars');
+// app.engine('handlebars', handlebars.engine());
+// app.set('views', __dirname+'/views');
+// app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use('/', viewRouter);
-app.use('/api/productos', productRouter);
+// app.use(express.json());
+// app.use('/', viewRouter);
+// app.use('/api/productos', productRouter);
 
 // app.use(express.static(__dirname+'/public'));
 
