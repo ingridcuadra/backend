900c417b2fe1c394da7172f6b0241042e39db7af
Ingrid Cuadra <ingrid_cuadra@outlook.es>
2023-01-20 21:09:43 -0300

    Se borraron los comentarios innecesarios

--- src/app.js
+++ src/app.js
@@ -15,39 +15,3 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/productos', productRouter);
-
-// import express from 'express';
-// import __dirname from './utils.js';
-// import productRouter from './routes/productos.router.js';
-// import handlebars from 'express-handlebars';
-// import viewRouter from './routes/views.router.js';
-
-// const app = express();
-// const server = app.listen(8080, ()=>console.log("Escuchando Express :)"));
-
-// app.use((req,res,next)=>{
-//     console.log("Petición recibida");
-//     next();
-// })
-
-// app.engine('handlebars', handlebars.engine());
-// app.set('views', __dirname+'/views');
-// app.set('view engine', 'handlebars');
-
-// app.use(express.json());
-// app.use('/', viewRouter);
-// app.use('/api/productos', productRouter);
-
-// app.use(express.static(__dirname+'/public'));
-
-//Pug 
-///app.set('views', __dirName + '/views')
-///app.set('view engine', 'pug')
-///app.use('/', productRouter);
-///app.use('/productos', productRouter);
-
-//Ejs
-///app.set('views', __dirName + '/views')
-///app.set('view engine', 'ejs')
-///app.use('/', productRouter);
-///app.use('/productos', productRouter);
